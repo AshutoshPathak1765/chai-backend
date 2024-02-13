9fd95b40be56f33635511c6d3615ec7e0e663690
Ashutosh Kumar Pathak <ashutoshpathak1765@gmail.com>
2024-02-13 10:56:07 +0530

    utils for api response and api error handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 5000;
+    app.on("error", (error) => {
+      console.log("EXPRESS ERROR :", error);
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("\n MONGODB connection failed !!!", error);
+  });
